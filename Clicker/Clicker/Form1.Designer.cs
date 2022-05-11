22c1d57bf4fa147e1d7942ad43360de12ad63161
Edwin Piątek <92691520+notedwinpiatek@users.noreply.github.com>
2022-05-11 11:52:56 +0200

    jednak teraz XD

---
+++ Clicker/Clicker/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.buttonLevelTextBox = new System.Windows.Forms.TextBox();
+            this.frequencyammount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.frequency = new System.Windows.Forms.Button();
+            this.quantity = new System.Windows.Forms.Button();
+            this.quantityammount = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Name = "label1";
+            // 
+            // upgradeButton
+            // 
+            resources.ApplyResources(this.upgradeButton, "upgradeButton");
+            this.upgradeButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.upgradeButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.UseVisualStyleBackColor = false;
+            this.upgradeButton.Click += new System.EventHandler(this.UpgradeButton_Click);
+            // 
+            // buttonLevelTextBox
+            // 
+            resources.ApplyResources(this.buttonLevelTextBox, "buttonLevelTextBox");
+            this.buttonLevelTextBox.Name = "buttonLevelTextBox";
+            this.buttonLevelTextBox.ReadOnly = true;
+            // 
+            // frequencyammount
+            // 
+            resources.ApplyResources(this.frequencyammount, "frequencyammount");
+            this.frequencyammount.Name = "frequencyammount";
+            this.frequencyammount.ReadOnly = true;
+            this.frequencyammount.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frequency
+            // 
+            resources.ApplyResources(this.frequency, "frequency");
+            this.frequency.Name = "frequency";
+            this.frequency.UseVisualStyleBackColor = true;
+            this.frequency.Click += new System.EventHandler(this.A1UpgradeInterval_Click);
+            // 
+            // quantity
+            // 
+            resources.ApplyResources(this.quantity, "quantity");
+            this.quantity.Name = "quantity";
+            this.quantity.UseVisualStyleBackColor = true;
+            this.quantity.Click += new System.EventHandler(this.A1UpgradeAmount_Click);
+            // 
+            // quantityammount
+            // 
+            resources.ApplyResources(this.quantityammount, "quantityammount");
+            this.quantityammount.Name = "quantityammount";
+            this.quantityammount.ReadOnly = true;
+            this.quantityammount.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.A1Tick);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.quantityammount);
+            this.Controls.Add(this.quantity);
+            this.Controls.Add(this.frequency);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.frequencyammount);
+            this.Controls.Add(this.buttonLevelTextBox);
+            this.Controls.Add(this.upgradeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button upgradeButton;
+        private TextBox buttonLevelTextBox;
+        private TextBox frequencyammount;
+        private Label label2;
+        private Button frequency;
+        private Button quantity;
+        private TextBox quantityammount;
+        private System.Windows.Forms.Timer timer1;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Clicker/Clicker/Form1.cs
@@ -0,0 +1,89 @@
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        int cash;
+        int buttonLevel;
+        int A1Ammount;
+        int A1Interval;
+        public Form1()
+        {
+            InitializeComponent();
+            cash = 0;
+            buttonLevel = 1;
+            A1Ammount = 10;
+            A1Interval = 0;
+            frequencyammount.Text = A1Ammount.ToString();
+            quantityammount.Text = A1Interval.ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cash += (int)Math.Pow(10, buttonLevel-1);
+            label1.Text = "Money: $"+cash.ToString();
+        }
+
+        private void UpgradeButton_Click(object sender, EventArgs e)
+        {
+            int upgradeCost = (int)Math.Pow(10, buttonLevel);
+            if (cash >= upgradeCost)
+            {
+                buttonLevel++;
+                buttonLevelTextBox.Text = buttonLevel.ToString();
+                cash -= upgradeCost;
+                label1.Text = "Money: $" + cash.ToString();
+                string nextUpgradeCost = "($" + Math.Pow(10, buttonLevel).ToString() + ")";
+                upgradeButton.Text = "Upgrade\n" + nextUpgradeCost;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void A1UpgradeAmount_Click(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void A1UpgradeInterval_Click(object sender, EventArgs e)
+        {
+            int upgradeCost = A1Interval * 100;
+            if (cash >= upgradeCost)
+            {
+                quantityammount.Text = A1Interval.ToString();
+                if (!quantity.Enabled)
+                    quantity.Enabled = true;
+                cash -= upgradeCost;
+
+            }
+        }
+
+        private void A1Tick(object sender, EventArgs e)
+        {
+            cash += A1Ammount;
+            label1.Text = "Money: $" + cash.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
